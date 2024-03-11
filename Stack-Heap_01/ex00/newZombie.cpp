5de3628eba3cf581a45f387094ad2eac1adbd61d
Burak Karlıdağ <burakkarlidag23@gmail.com>
2024-03-11 21:45:27 +0300

    Part1 Cpp

--- Stack-Heap_01/ex00/newZombie.cpp
+++
@@ -1,8 +0,0 @@
-#include "zombie.hpp"
-
-zombie* newZombie(std::string name)
-{
-
-	zombie *zombi = new zombie(name);
-	return (zombi);
-}
--- Stack-Heap_01/ex00/randomChump.cpp
+++
@@ -1,7 +0,0 @@
-#include "zombie.hpp"
-
-void random(std::string name)
-{
-	zombie zombie(name);
-    zombie.announce();
-}
